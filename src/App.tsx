26c59c2462547c397f99e028cf8dc063ad38384d
Billy <billyfranklim@gmail.com>
2024-03-13 23:43:03 -0300

    ✨ feat(app): add show/hide password functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import {
   MdPersonOutline,
   MdLockOutline,
   MdInput,
+  MdVisibility,
+  MdVisibilityOff,
 } from "react-icons/md";
 
 import { FaApple, FaGithub, FaTwitter } from "react-icons/fa6";
@@ -14,6 +16,7 @@ export default function App() {
 
   const [currentImage, setCurrentImage] = useState(0);
   const [bannerOpen, setBannerOpen] = useState(true);
+  const [showPassword, setShowPassword] = useState(false);
 
   return (
     <div className="flex h-screen w-screen justify-center">
@@ -22,7 +25,7 @@ export default function App() {
           bannerOpen ? "w-2/3" : "w-0"
         }  hidden lg:block md:block`}
         style={{
-          backgroundImage: ` url(${images[currentImage]})`,
+          backgroundImage: `url(${images[currentImage]})`,
           transition: "background-image 0.5s ease-in-out",
         }}
       >
@@ -30,31 +33,32 @@ export default function App() {
           <div className="relative h-full">
             <div className="absolute w-full px-5 py-5 flex justify-start">
               <button
-                className="w-10 h-10 flex items-center justify-center border rounded-full text-black bg-transparent border-black hover:bg-gray-700 hover:text-white"
+                className="w-10 h-10 flex items-center justify-center border rounded-full text-black bg-transparent border-black hover:border-white hover:text-white"
                 onClick={() => setBannerOpen(!bannerOpen)}
               >
                 <IoIosArrowBack />
               </button>
             </div>
-            <div className="absolute bottom-16 mx-5 flex flex-col items-center justify-center text-white">
+            <div className="absolute bottom-14 mx-5 flex flex-col items-center justify-center text-white">
               <div className="flex flex-col gap-4">
-                <div className="p-2 flex justify-start items-center">
-                  <span className="px-4 py-2 rounded-full bg-blue-700">
+                <div className="flex justify-start items-center">
+                  <span className="px-4 py-2 rounded-full bg-blue-700 text-sm">
                     Cursos
                   </span>
                 </div>
-                <div className="px-2 flex flex-col gap-4">
-                  <h3 className="text-2xl font-bold my-5">
+                <div className="flex flex-col">
+                  <h3 className="text-base font-semibold my-4">
                     Plataforma de cursos completa
                   </h3>
-                  <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Corrupti, veritatis eveniet cupiditate illo eius beatae enim
-                    dolorum doloremque totam incidunt maxime. Id voluptate
-                    aspernatur dolorum, sint alias sapiente earum blanditiis!
+                  <p className="font-medium text-gray-400">
+                    Lorem ipsum nisl etiam himenaeos ligula augue vehicula
+                    gravida tincidunt, etiam magna sapien gravida sodales sed
+                    vel pulvinar suspendisse, morbi mi proin urna ornare posuere
+                    donec aptent. orci vivamus primis fusce lacinia libero
+                    nostra aliquam vestibulum
                   </p>
                 </div>
-                <div className="px-2 flex items-center justify-center gap-4">
+                <div className="flex items-center justify-center gap-4 mt-10">
                   <div className="flex items-center justify-center gap-3 w-1/3">
                     {Array(3)
                       .fill(0)
@@ -72,7 +76,7 @@ export default function App() {
                     <IoIosArrowBack
                       size={30}
                       className={`cursor-pointer ${
-                        currentImage === 0 ? "text-gray-500" : "text-white"
+                        currentImage === 0 ? "text-gray-400" : "text-white"
                       }`}
                       onClick={() => {
                         if (currentImage !== 0) {
@@ -86,7 +90,7 @@ export default function App() {
                       size={30}
                       className={`cursor-pointer ${
                         currentImage === images.length - 1
-                          ? "text-gray-500"
+                          ? "text-gray-400"
                           : "text-white"
                       }`}
                       onClick={() => {
@@ -103,14 +107,14 @@ export default function App() {
         )}
       </div>
       <div
-        className={`h-full px-5 md:w-full w-full flex justify-center ${
+        className={`h-full px-10 md:w-full w-full flex justify-center ${
           bannerOpen ? "md:w-1/3 w-full lg:w-1/3" : "w-full lg:w-5/12"
         } `}
       >
         {!bannerOpen && (
           <div className="absolute left-5 hidden lg:block md:block">
             <button
-              className="mt-5 bg-blue-500 text-white rounded-full w-10 h-10 hover:bg-gray-700 flex items-center justify-center"
+              className="mt-5 bg-blue-500 text-white rounded-full w-10 h-10 hover:bg-blue-700 flex items-center justify-center"
               onClick={() => setBannerOpen(true)}
             >
               <IoIosArrowForward />
@@ -126,8 +130,10 @@ export default function App() {
             </a>
           </div>
           <div className="flex flex-col mt-10">
-            <h1 className="text-3xl font-bold">Boas-vindas!</h1>
-            <p className="mt-5">Entre ultilizando umas das opções abaixo.</p>
+            <h1 className="text-3xl font-semibold">Boas-vindas!</h1>
+            <p className="text-lg font-medium text-gray-400 mt-5">
+              Entre ultilizando umas das opções abaixo.
+            </p>
           </div>
           <div className="flex w-full gap-2 mt-10">
             <div className="border-black w-10 h-10 flex flex-1 justify-center items-center border rounded-md hover:bg-gray-100 cursor-pointer">
@@ -145,12 +151,12 @@ export default function App() {
           </div>
           <div className="flex items-center mt-10">
             <div className="flex-1 border-t border-gray-300"></div>
-            <div className="px-2 text-gray-500">ou</div>
+            <div className="px-2 text-gray-400">ou</div>
             <div className="flex-1 border-t border-gray-300"></div>
           </div>
 
           <div className="mt-10">
-            <label className="block text-gray-600">Usuário</label>
+            <label className="block text-gray-400">Usuário</label>
             <div className="mt-2 relative">
               <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                 <MdPersonOutline size={20} />
@@ -164,22 +170,32 @@ export default function App() {
             </div>
           </div>
           <div className="mt-4">
-            <label className="block text-gray-600">Senha</label>
+            <label className="block text-gray-400">Senha</label>
             <div className="mt-2 relative">
               <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                 <MdLockOutline size={20} />
               </span>
               <input
-                type="password"
+                type={showPassword ? "text" : "password"}
                 className="w-full p-2 border border-gray-800 rounded-lg pl-10 focus:outline-none"
                 placeholder="Senha"
                 autoComplete="off"
               />
+              <span
+                className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <MdVisibilityOff size={20} />
+                ) : (
+                  <MdVisibility size={20} />
+                )}
+              </span>
             </div>
           </div>
           <div className="mt-4 flex items-center">
             <input type="checkbox" id="stayConnected" className="mr-2" />
-            <label htmlFor="stayConnected" className="text-gray-600">
+            <label htmlFor="stayConnected" className="text-gray-400">
               Manter conectado
             </label>
           </div>
@@ -188,7 +204,7 @@ export default function App() {
             Entrar
           </button>
           <div className="mt-6 flex justify-center">
-            <a href="#" className="text-center text-sm text-gray-600">
+            <a href="#" className="text-center text-sm text-gray-400">
               Esqueceu sua senha ? {""}
               <span className="text-indigo-600">Recuperar senha</span>
             </a>
