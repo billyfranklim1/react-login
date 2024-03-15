f8c89a7b30321e818b703851aecc3490711ae32d
Billy <billyfranklim@gmail.com>
2024-03-14 23:26:02 -0300

    ✨ feat(index.html): Add preload links for images and change title to Login

--- src/screens/login/components/Banner/BannerContainer/index.tsx
+++ src/screens/login/components/Banner/BannerContainer/index.tsx
@@ -1,30 +1,33 @@
+import React from 'react';
 import CloseBannerButton from "../CloseBannerButton";
 import Content from "../Content";
 import ImagePagination from "../ImagePagination";
 import { useBanner } from "../../../../../contexts/Banner/BannerContext";
+import classNames from 'classnames';
 
 export default function BannerContainer() {
   const { isOpen, images, currentImage } = useBanner();
 
+  const containerClasses = classNames(
+    'bg-cover hidden lg:block md:block', {
+      'w-2/3': isOpen,
+      'w-0': !isOpen,
+    }
+  );
+
   return (
     <div
-      className={`bg-cover  ${
-        isOpen ? "w-2/3" : "w-0"
-      } hidden lg:block md:block`}
-      style={{
-        backgroundImage: `url(${images[currentImage]})`,
-      }}
+      className={containerClasses}
+      style={{ backgroundImage: `url(${images[currentImage]})` }}
     >
       {isOpen && (
-        <>
-          <div className="h-full flex flex-col justify-between p-5">
-            <CloseBannerButton />
-            <div className="  text-white flex flex-col justify-end">
-              <Content />
-              <ImagePagination />
-            </div>
+        <div className="h-full flex flex-col justify-between p-5">
+          <CloseBannerButton />
+          <div className="text-white flex flex-col justify-end">
+            <Content />
+            <ImagePagination />
           </div>
-        </>
+        </div>
       )}
     </div>
   );
